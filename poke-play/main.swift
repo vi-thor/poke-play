dd76efb492c95fa13700f604e2e49f6a8494743b
Vitor <vitor.h.goncalves23@gmail.com>
2023-07-13 19:23:58 +0100

    planing project

--- poke-play/main.swift
+++ poke-play/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
-
+/**
+todos
+ 1 create entity pokemon
+ 2 create menu for navigation
+ 3 create
+ 4 mock 3 pokemons default
+ 5 create own pokemon
+ 6 fight
+    6.1 choose user pokemon
+    6.2 choose enemy pokemon
+    6.3 create finish
+ */
